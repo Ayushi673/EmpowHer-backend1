76e3785a7b437240ac59b386ffda48371ceaeb18
Ayushi673 <dasayushi16@gmail.com>
2021-06-05 20:56:07 +0530

    betterment

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const importData= require('./legal.json');
 let port=process.env.PORT || 3000;
 
 app.get("/",(req,res)=>{
-    res.send("Hello world");
-})
-
-app.get("/legal",(req,res)=>{
     res.send(importData);
 })
+
 app.listen(port,()=>{
     console.log(`App running on port http://localhost:${port}`)
 })
